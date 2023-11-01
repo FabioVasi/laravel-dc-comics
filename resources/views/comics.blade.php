6a77add6935948efad3f7638129262795318dbae
FabioVasi <vasifabio96@gmail.com>
2023-11-01 21:48:51 +0100

    end

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -13,6 +13,9 @@
                         <div class="card-body">
                             <h3>{{$comic->title}}</h3>
                             <a class="btn btn-primary" href="{{route('guests.comics.show', $comic->id)}}">View</a>
+                            <p>{{$comic->description}}</p>
+                            <h5>{{$comic->price}}</h5>
+                            <h5>{{$comic->series}}</h5>
                         </div>
                     </div>
                 </div>
