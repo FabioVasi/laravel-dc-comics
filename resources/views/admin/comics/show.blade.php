baca14bc7cbcf2c3ed2c5d9e20e675f166cdbc5d
FabioVasi <vasifabio96@gmail.com>
2023-11-02 19:56:32 +0100

    fix show

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -7,17 +7,20 @@
     <div class="container">
         
         <h2>{{$dccomic->id}}</h2>
-        <tr class="">
-            <td scope="row" >{{$dccomic->id}}</td>
-            <td><img width="100" src="{{asset('storage/' . $dccomic->thumb)}}" alt=""></td>
-            <td>{{$dccomic->title}}</td>
-            <td>{{$dccomic->price}}</td>
-            <td>{{$dccomic->series}}</td>
-            <td>
-                <a href="#" class="btn btn-primary">View</a>
-                Edit/Delete
-            </td>
-        </tr>
+        
+        <div class="row">
+            <div class="col-4" >{{$dccomic->id}}
+                <div class="card">
+                    <img class="card-img-top" width="100" src="{{asset('storage/' . $dccomic->thumb)}}" alt="">
+                    <div class="card-body">
+                        <h3>{{$dccomic->title}}</h3>
+                        <h2>{{$dccomic->price}}</h2>
+                        <h2>{{$dccomic->series}}</h2>
+                        <a href="#" class="btn btn-primary">View</a>Edit/Delete
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 
